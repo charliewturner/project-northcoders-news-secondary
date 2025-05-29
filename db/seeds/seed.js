6f7406365cf78fb43444966b3ab3271df8902d7a
charliewturner <c.w.turner@hotmail.com>
2025-05-29 15:47:21 +0100

    Upload file passing all seed tests

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,19 +1,20 @@
 const db = require("../connection");
 const data = require("../data/development-data/index");
 const format = require("pg-format");
+const { convertTimestampToDate } = require("./utils");
 
 const createTopicsTable = format(`
   CREATE TABLE topics (
   slug VARCHAR(50) UNIQUE PRIMARY KEY,
   description VARCHAR(512),
-  img_url VARCHAR(512)
+  img_url VARCHAR(1000)
 );`);
 
 const createUsersTable = format(`
   CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     name VARCHAR(50),
-    avatar_url VARCHAR(512)
+    avatar_url VARCHAR(1000)
   );`);
 
 const createArticlesTable = format(`
@@ -25,7 +26,9 @@ CREATE TABLE articles (
   body TEXT,
   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   votes INT DEFAULT 0,
-  article_img_url VARCHAR(512)
+  article_img_url VARCHAR(1000),
+  FOREIGN KEY (topic) REFERENCES topics (slug),
+FOREIGN KEY (author) REFERENCES users (username)
 );`);
 
 const createCommentsTable = format(`
@@ -39,43 +42,6 @@ CREATE TABLE comments (
   FOREIGN KEY (author) REFERENCES users (username),
   FOREIGN KEY (article_id) REFERENCES articles (article_id)
 );`);
-
-// const updateTopicsTable = format(
-//   `
-//   INSERT INTO topics
-//     (slug, description, img_url)
-//   VALUES
-//     %L
-//   RETURNING *;`,
-//   nestedArrOfValues
-// );
-// const updateUsersTable = format(
-//   `
-//   INSERT INTO users
-//     (username, name, avatar_url)
-//   VALUES
-//     %L
-//   RETURNING *;`,
-//   nestedArrOfValues
-// );
-// const updateArticlesTable = format(
-//   `
-//   INSERT INTO articles
-//     (title, topic, author, body, created_at, votes, article_img_url)
-//   VALUES
-//     %L
-//   RETURNING *;`,
-//   nestedArrOfValues
-// );
-
-// const updateCommentsTable = format(
-//   `INSERT INTO comments
-//     (article_id, body, votes, author, created_at)
-//   VALUES
-//     %L
-//   RETURNING *;`,
-//   nestedArrOfValues
-// );
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
   // For this task you should create tables for topics, users, articles, and comments
@@ -96,6 +62,92 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
     .then(() => {
       console.log("Tables created successfully!");
       return db.query(createCommentsTable);
+    })
+    .then(() => {
+      const topicValues = topicData.map(({ slug, description, img_url }) => [
+        slug,
+        description,
+        img_url,
+      ]);
+      const updateTopicsTable = format(
+        `INSERT INTO topics
+          (slug, description, img_url)
+        VALUES
+         %L
+         RETURNING *;`,
+        topicValues
+      );
+      return db.query(updateTopicsTable);
+    })
+    .then(() => {
+      const userValues = userData.map(({ username, name, avatar_url }) => [
+        username,
+        name,
+        avatar_url,
+      ]);
+      const updateUsersTable = format(
+        `
+        INSERT INTO users
+        (username, name, avatar_url)
+         VALUES
+          %L
+           RETURNING *;`,
+        userValues
+      );
+      return db.query(updateUsersTable);
+    })
+    .then(() => {
+      const articleValues = articleData.map((article) => {
+        const convertedArticle = convertTimestampToDate(article);
+        return [
+          convertedArticle.title,
+          convertedArticle.topic,
+          convertedArticle.author,
+          convertedArticle.body,
+          convertedArticle.created_at,
+          convertedArticle.votes,
+          convertedArticle.article_img_url,
+        ];
+      });
+
+      const updateArticlesTable = format(
+        `INSERT INTO articles
+        (title, topic, author, body, created_at, votes,      article_img_url)
+        VALUES
+        %L
+          RETURNING *;`,
+        articleValues
+      );
+      return db.query(updateArticlesTable);
+    })
+    .then(() => {
+      const commentValues = commentData.map((comment) => {
+        const convertedComment = convertTimestampToDate(comment);
+        return [
+          convertedComment.article_id,
+          convertedComment.body,
+          convertedComment.votes,
+          convertedComment.author,
+          convertedComment.created_at,
+        ];
+      });
+
+      const updateCommentsTable = format(
+        `INSERT INTO comments
+          (article_id, body, votes, author, created_at)
+         VALUES
+         %L
+         RETURNING *;`,
+        commentValues
+      );
+
+      return db.query(updateCommentsTable);
+    })
+    .then(() => {
+      console.log("Tables successfully updated!");
+    })
+    .catch((err) => {
+      console.log("Error during seeding: " + err);
     });
 };
 
