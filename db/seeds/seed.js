7e49e12d889a47af5a549725d8c5c7940798cebf
charliewturner <c.w.turner@hotmail.com>
2025-05-29 15:05:42 +0100

    upload kata 2 progress

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,23 +2,21 @@ const db = require("../connection");
 const data = require("../data/development-data/index");
 const format = require("pg-format");
 
-const createTopicsTable = format(`DROP TABLE IF EXISTS topics;
-
-CREATE TABLE topics (
+const createTopicsTable = format(`
+  CREATE TABLE topics (
   slug VARCHAR(50) UNIQUE PRIMARY KEY,
   description VARCHAR(512),
   img_url VARCHAR(512)
 );`);
 
-const createUsersTable = format(`DROP TABLE IF EXISTS users;
-
+const createUsersTable = format(`
   CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     name VARCHAR(50),
     avatar_url VARCHAR(512)
   );`);
 
-const createArticlesTable = format(`DROP TABLE IF EXISTS articles;
+const createArticlesTable = format(`
 CREATE TABLE articles (
   article_id SERIAL PRIMARY KEY,
   title VARCHAR(512),
@@ -30,21 +28,76 @@ CREATE TABLE articles (
   article_img_url VARCHAR(512)
 );`);
 
-const createCommentsTable = format(`DROP TABLE IF EXISTS comments;
+const createCommentsTable = format(`
 CREATE TABLE comments (
   comment_id SERIAL PRIMARY KEY,
   article_id INT,
-  FOREIGN KEY (article_id) REFERENCES articles (article_id),
   body TEXT,
   votes INT default 0,
   author VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (author) REFERENCES users (username),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  FOREIGN KEY (article_id) REFERENCES articles (article_id)
 );`);
+
+// const updateTopicsTable = format(
+//   `
+//   INSERT INTO topics
+//     (slug, description, img_url)
+//   VALUES
+//     %L
+//   RETURNING *;`,
+//   nestedArrOfValues
+// );
+// const updateUsersTable = format(
+//   `
+//   INSERT INTO users
+//     (username, name, avatar_url)
+//   VALUES
+//     %L
+//   RETURNING *;`,
+//   nestedArrOfValues
+// );
+// const updateArticlesTable = format(
+//   `
+//   INSERT INTO articles
+//     (title, topic, author, body, created_at, votes, article_img_url)
+//   VALUES
+//     %L
+//   RETURNING *;`,
+//   nestedArrOfValues
+// );
+
+// const updateCommentsTable = format(
+//   `INSERT INTO comments
+//     (article_id, body, votes, author, created_at)
+//   VALUES
+//     %L
+//   RETURNING *;`,
+//   nestedArrOfValues
+// );
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
   // For this task you should create tables for topics, users, articles, and comments
 
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(
+      `DROP TABLE IF EXISTS comments; DROP TABLE IF EXISTS articles;DROP TABLE IF EXISTS users;DROP TABLE IF EXISTS topics;`
+    )
+    .then(() => {
+      return createTopicsTable();
+    })
+
+    .then(() => {
+      return createUsersTable();
+    })
+
+    .then(() => {
+      return createArticlesTable();
+    })
+
+    .then(() => {
+      return createCommentsTable();
+    });
 };
 module.exports = seed;
