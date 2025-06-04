726d4f60b01ef9178c6259d722a512571448fb2d
charliewturner <c.w.turner@hotmail.com>
2025-06-04 15:31:43 +0100

    Complete kata 2 with tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,10 @@ const endpointsJson = require("../endpoints.json");
 
 const request = require("supertest");
 const db = require("../db/connection.js");
-const seed = require("../db/seeds/run-seed.js");
+const seed = require("../db/seeds/seed.js");
 const testData = require("../db/data/test-data");
 const app = require("../app.js");
+const { forEach } = require("../db/data/development-data/articles.js");
 /* Set up your beforeEach & afterAll functions here */
 
 beforeEach(() => {
@@ -17,12 +18,30 @@ afterAll(() => {
 });
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topics. Each topic is an object with a slug and a description key", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        console.log(body.topics);
+        expect(Array.isArray(body.topics)).toEqual(true);
+        expect(Object.keys(body)).toEqual(["topics"]);
+
+        body.topics.forEach((topic) => {
+          expect(topic.hasOwnProperty("slug")).toEqual(true);
+          expect(topic.hasOwnProperty("description")).toEqual(true);
+        });
       });
   });
 });
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const { getAllTopics } = require("./controllers.js");
+const endpoints = require("./endpoints.json");
 
+app.get("/api", (request, response) => {
+  response.status(200).send({ endpoints });
+});
 app.get("/api/topics", getAllTopics);
 
 module.exports = app;
