9c6d1404a46e7efbc83a5b63203eea9ba7dee701
charliewturner <c.w.turner@hotmail.com>
2025-06-25 22:08:26 +0100

    Get comment information access working and begin generating comments on popup

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
-
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+console.log(ENV);
+console.log(process.env.PGPASSWORD);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
